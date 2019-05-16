48d76d03e092aee5ae78327a84e15c2204141e6e
Michael Lamparski <diagonaldevice@gmail.com>
2019-05-16 16:13:07 -0400

    add edition tests and travis config

---
+++ test-crates/edition-2015/main.rs
@@ -0,0 +1,16 @@
+#[macro_use]
+extern crate newtype_ops;
+
+#[derive(Debug, PartialEq)]
+pub struct Foo(pub i32);
+newtype_ops! { [Foo] integer {:=} {^&}Self {^&}{Self i32} }
+
+#[derive(Debug, PartialEq)]
+pub struct Bar(pub f32);
+newtype_ops! { [Bar] arithmetic {:=} {^&}Self {^&}{Self f32} }
+
+fn main() {
+    assert_eq!(Foo(2) + Foo(3), Foo(5));
+    assert_eq!(Foo(2) | Foo(3), Foo(3));
+    assert_eq!(Bar(2.0) - Bar(3.0), Bar(-1.0));
+}
